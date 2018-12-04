b6423eb28f6c078e047b154b468e89edd58a269a
xRuiAlves <up201606746@fe.up.pt>
2018-12-04 20:36:48 +0000

    Added project's boilerplate

---
+++ src/main/java/api/endpoints/ReverseService.java
@@ -0,0 +1,19 @@
+package api.endpoints;
+
+import javax.validation.constraints.NotNull;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import org.springframework.stereotype.Service;
+
+@Service
+@Path("/reverse")
+public class ReverseService {
+
+    @GET
+    @Produces("text/plain")
+    public String reverse(@QueryParam("data") @NotNull String data) {
+        return new StringBuilder(data).reverse().toString();
+    }
+}
---
+++ src/test/java/api/ApplicationTests.java
@@ -0,0 +1,43 @@
+package api;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class ApplicationTests {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void hello() {
+        ResponseEntity<String> entity = this.restTemplate.getForEntity("/hello",
+                String.class);
+        assertThat(entity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(entity.getBody()).isEqualTo("Hello from Spring");
+    }
+
+    @Test
+    public void reverse() {
+        ResponseEntity<String> entity = this.restTemplate
+                .getForEntity("/reverse?data=regit", String.class);
+        assertThat(entity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(entity.getBody()).isEqualTo("tiger");
+    }
+
+    @Test
+    public void validation() {
+        ResponseEntity<String> entity = this.restTemplate.getForEntity("/reverse",
+                String.class);
+        assertThat(entity.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+    }
+}
