36e0d89f92cef2b29b251c237ded5fb4af1dbf62
miguelpduarte <miguelpduarte98@gmail.com>
2019-02-13 11:55:26 +0000

    Added db connection (corrected structure)

---
+++ src/db_controller.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const MONGO_URI = process.env.MONGO_URI || "mongodb://mongo:27017";
+
+const DB_NAME = (process.env.NODE_ENV === "test" ? "test-db" : "nijobs-db");
+
+const options = {
+    useNewUrlParser: true,
+    dbName: process.env.MONGO_URI ? undefined : DB_NAME,
+};
+
+const db_connection_promise = mongoose.connect(MONGO_URI, options);
+
+module.exports = db_connection_promise;
---
+++ src/index.js
@@ -0,0 +1,32 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+
+const db_connection = require("./db_controller");
+
+const PORT = process.env.PORT || 3000;
+
+if (process.env.NODE_ENV === "production") {
+    if (!process.env.MONGO_URI) {
+        console.error("MONGO_URI was not defined in .env file and we are in production environment! Aborting!");
+        process.exit(88);
+    } else {
+        console.warn("MONGO_URI was not defined in .enf file, falling back to localhost defaults.");
+    }
+}
+
+console.log("Connecting to Mongo DB...");
+
+db_connection
+    .then(() => {
+        app.listen(PORT, () => console.log(`App listening on internal port ${PORT}`));
+    })
+    .catch((err) => {
+        console.error("Error connecting to the DB (aborting): ", err);
+        process.exit(44);
+    });
+
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
