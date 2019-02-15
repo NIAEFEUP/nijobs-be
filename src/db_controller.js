dc6658b9e88f754497b15b37691111205064768f
miguelpduarte <miguelpduarte98@gmail.com>
2019-02-15 10:06:44 +0000

    Solved mongoose using deprecated mongo functions

--- src/db_controller.js
+++ src/db_controller.js
@@ -11,6 +11,7 @@ const DB_NAME = (process.env.NODE_ENV === "test" ? "test-db" : "nijobs-db");
 const options = {
     useNewUrlParser: true,
     dbName: process.env.MONGO_URI ? undefined : DB_NAME,
+    useCreateIndex: true,
 };
 
 const db_connection_promise = mongoose.connect(MONGO_URI, options);
