47c7fa23dcf5ed7b42ebc3655f1b29d6f74f2866
miguelpduarte <miguelpduarte98@gmail.com>
2019-02-25 10:27:26 +0000

    Added required to some fields of Ad and 'foreign key' to Company

--- src/models/Ad.js
+++ src/models/Ad.js
@@ -1,17 +1,19 @@
 const mongoose = require("mongoose");
-const { Schema } = mongoose;
+const { Schema, Types } = mongoose;
 
 const AdSchema = new Schema({
-    title: {type: String},
-    publishDate: {type: Date},
-    endDate: {type: Date},
+    title: {type: String, required: true},
+    publishDate: {type: Date, required: true},
+    endDate: {type: Date, required: true},
+    // Shouldn't this just be a String? Simplifies companies wanting to insert ranges and whatnot
     jobDuration: {type: Number},
     jobStartDate: {type: Date},
-    description: {type: String, maxlength: 1500},
-    contacts: {type: String},
+    description: {type: String, maxlength: 1500, required: true},
+    contacts: {type: String, required: true},
     isPaid: {type: Boolean},
     vacancies: {type: Number},
     isHidden: {type: Boolean},
+    owner: {type: Types.ObjectId, ref: "Company", required: true},
 });
 
 const Ad = mongoose.model("Ad", AdSchema);
