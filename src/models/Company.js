6d4b45b94b67887591c80d2f9f93096dcf40b511
miguelpduarte <miguelpduarte98@gmail.com>
2019-08-17 18:15:19 +0100

    Improve company schema

--- src/models/Company.js
+++ src/models/Company.js
@@ -2,11 +2,24 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const CompanySchema = new Schema({
-    name: {type: String},
-    // Should this instead be an array of key value strings?
-    // Like email: john@doe.com, phone: 9139porfavornaoincomode, etc
-    contacts: {type: String},
-    bio: {type: String, maxlength: 1500},
+    name: {
+        type: String,
+        required: true,
+    },
+    contacts: {
+        type: Map,
+        of: String,
+        required: true,
+        validate: [
+            (val) => val.size >= 1,
+            "There must be at least one contact"
+        ]
+    },
+    bio: {
+        type: String,
+        maxlength: 1500,
+        required: true,
+    },
 });
 
 const Company = mongoose.model("Company", CompanySchema);
