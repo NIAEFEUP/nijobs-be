451a5680fecdd09742a8631381f1f7d7a6862f60
miguelpduarte <miguelpduarte98@gmail.com>
2019-08-20 20:52:35 +0100

    Add linting to tests
    - Complements #28, this was probably forgotten or would have been
    included there
    - Fix linting errors. This was mostly automatic but there was also some
    manual work relative to following mocha's suggestions for using
    async/await in tests for the auth tests
    (https://mochajs.org/#using-async-await)

--- test/common.js
+++ test/common.js
@@ -1,8 +1,10 @@
-//// Base file for unit test utilities
+//----------------------------------
+// Base file for unit test utilities
+//----------------------------------
 
 require("dotenv-flow").config();
 
-//During the test the env variable must be set to test
+// During the test the env variable must be set to test
 if (process.env.NODE_ENV !== "test") {
     console.error("Entered test files without being in the test environment, aborting!");
     process.exit(55);
@@ -13,12 +15,12 @@ before("Waiting for DB initialization", async () => {
     await require("../src/db_controller");
 });
 
-//Require the dev-dependencies
+// Require the dev-dependencies
 const chai = require("chai");
 const chaiHttp = require("chai-http");
 chai.use(chaiHttp);
 const should = chai.should();
-const {server, app} = require("../src/index");
+const { server, app } = require("../src/index");
 // module.exports.server = server;
 const request = () => chai.request(server);
 const agent = () => chai.request.agent(app);
@@ -27,8 +29,6 @@ module.exports.should = should;
 module.exports.request = request;
 module.exports.agent = agent;
 
-module.exports.sleep = ms => {
-    return new Promise(resolve => {
-        setTimeout(resolve, ms);
-    });
-};
+module.exports.sleep = (ms) => new Promise((resolve) => {
+    setTimeout(resolve, ms);
+});
