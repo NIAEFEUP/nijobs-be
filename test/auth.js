3480f2cc6e14c513d8edc95b992bd11ca0aa5db7
Guilherme Silva <guilhermejosesilva@gmail.com>
2019-08-22 14:42:26 +0100

    Auth endpoint tests refactor to include a better description

--- test/auth.js
+++ test/auth.js
@@ -7,134 +7,127 @@ const {
 const ERROR_TYPES = require("../src/routes/errors/errorHandler");
 const Account = require("../src/models/Account");
 
-describe("Register endpoint test", () => {
+describe("# Authentication endpoint tests", () => {
+    describe("Register endpoint", () => {
 
-    before("Clearing accounts", () => Account.deleteMany({}));
-    after("Clearing accounts", () => Account.deleteMany({}));
+        describe("Validation tests", () => {
+            describe("Missing required fields", () => {
+                it("Should return a malformed request (missing username)", () => request()
+                    .post("/api/auth/register")
+                    .send({}).then((res) => {
+                        res.should.have.status(400);
+                        res.body.should.be.an("object");
+                        res.body.should.have.property("success").equal(false);
+                        res.body.should.have.property("reason").that.is.a("string");
+                        res.body.should.have.property("error_code").equal(ERROR_TYPES.MISSING_FIELD);
+                    }));
 
+                it("Should return a malformed request (missing password)", () => request()
+                    .post("/api/auth/register")
+                    .send({
+                        username: "user",
+                    }).then((res) => {
+                        res.should.have.status(400);
+                        res.body.should.be.an("object");
+                        res.body.should.have.property("success").equal(false);
+                        res.body.should.have.property("reason").that.is.a("string");
+                        res.body.should.have.property("error_code").equal(ERROR_TYPES.MISSING_FIELD);
+                    }));
+            });
+        });
 
-    it("Should return a malformed request (missing username)", async () => {
-        const res = await request()
-            .post("/api/auth/register")
-            .send({});
-
-        res.should.have.status(400);
-        res.body.should.be.an("object");
-        res.body.should.have.property("success").equal(false);
-        res.body.should.have.property("reason").that.is.a("string");
-        res.body.should.have.property("error_code").equal(ERROR_TYPES.MISSING_FIELD);
-    });
-
-    it("Should return a malformed request (missing password)", async () => {
-        const res = await request()
-            .post("/api/auth/register")
-            .send({
+        describe("Using valid input", () => {
+            before("Clearing accounts", () => Account.deleteMany({}));
+            after("Clearing accounts", () => Account.deleteMany({}));
+            const user = {
                 username: "user",
+                password: "password",
+            };
+
+            it("Should make a successful registration", async () => {
+
+                const res = await request()
+                    .post("/api/auth/register")
+                    .send(user);
+
+                res.should.have.status(200);
+                res.body.should.be.an("object");
+                res.body.should.have.property("success").equal(true);
+
+                const registered_user = await Account.findOne({ username: user.username });
+                should.exist(registered_user);
+                registered_user.should.have.property("username").equal(user.username);
             });
 
-        res.should.have.status(400);
-        res.body.should.be.an("object");
-        res.body.should.have.property("success").equal(false);
-        res.body.should.have.property("reason").that.is.a("string");
-        res.body.should.have.property("error_code").equal(ERROR_TYPES.MISSING_FIELD);
+            it("Stop registering of users with duplicate usernames", () => request()
+                .post("/api/auth/register")
+                .send(user).then((res) => {
+                    res.should.have.status(500);
+                    res.body.should.be.an("object");
+                    res.body.should.have.property("reason").that.is.a("string");
+                    res.body.should.have.property("success").equal(false);
+                    res.body.should.have.property("error_code").equal(ERROR_TYPES.DB_ERROR);
+                }));
+        });
     });
 
-    it("Sould make a successful registration", async () => {
-        const user = {
-            username: "user",
-            password: "password",
-        };
+    describe("Login/Logout enpoints", () => {
+        before("Adding user", () => {
+            this.agent = agent();
 
-        const res = await request()
-            .post("/api/auth/register")
-            .send(user);
+            this.user = {
+                username: "user",
+                password: "password",
+            };
 
-        res.should.have.status(200);
-        res.body.should.be.an("object");
-        res.body.should.have.property("success").equal(true);
+            return Account.create([this.user]);
+        });
 
-        const registered_user = await Account.findOne({ username: user.username });
-        should.exist(registered_user);
-        registered_user.should.have.property("username").equal(user.username);
-    });
-});
 
-describe("Using already resgistered user", () => {
-    before("Adding user", async () => {
-        this.agent = agent();
-
-        this.user = {
-            username: "user",
-            password: "password",
-        };
-
-        await Account.create([this.user]);
-    });
-
-    it("Stop registering of users with duplicate usernames", async () => {
-        const res = await request()
-            .post("/api/auth/register")
-            .send(this.user);
-
-        res.should.have.status(500);
-        res.body.should.be.an("object");
-        res.body.should.have.property("reason").that.is.a("string");
-        res.body.should.have.property("success").equal(false);
-        res.body.should.have.property("error_code").equal(ERROR_TYPES.DB_ERROR);
-    });
-
-    it("Should return forbidden when retrieving the information of the logged in user", async () => {
-        const res = await request()
+        it("Should return forbidden when retrieving the information of the logged in user", () => request()
             .get("/api/auth/login")
-            .send();
+            .send().then((res) => {
+                res.should.have.status(401);
+                res.body.should.be.an("object");
+                res.body.should.have.property("success").equal(false);
+                res.body.should.have.property("reason").that.is.a("string");
+            }));
 
-        res.should.have.status(401);
-        res.body.should.be.an("object");
-        res.body.should.have.property("success").equal(false);
-        res.body.should.have.property("reason").that.is.a("string");
-    });
 
-
-    it("Log in with registered account", async () => {
-        const res = await this.agent
+        it("Log in with registered account", () => this.agent
             .post("/api/auth/login")
-            .send(this.user);
+            .send(this.user).then((res) => {
+                res.should.have.cookie("connect.sid");
+                res.should.have.status(200);
+                res.body.should.be.an("object");
+                res.body.should.have.property("success").equal(true);
+            }));
 
-        res.should.have.cookie("connect.sid");
-        res.should.have.status(200);
-        res.body.should.be.an("object");
-        res.body.should.have.property("success").equal(true);
-    });
-
-    it("Get logged in user info", async () => {
-        const res = await this.agent
+        it("Get logged in user info", () => this.agent
             .get("/api/auth/login")
-            .send();
+            .send().then((res) => {
+                res.should.have.status(200);
+                res.body.should.be.an("object");
+                res.body.should.have.property("success").equal(true);
+                res.body.should.have.nested.property("data.username").equal(this.user.username);
+            }));
 
-        res.should.have.status(200);
-        res.body.should.be.an("object");
-        res.body.should.have.property("success").equal(true);
-        res.body.should.have.nested.property("data.username").equal(this.user.username);
-    });
-
-    it("Log out with registered account", async () => {
-        const res = await this.agent
+        it("Log out with registered account", () => this.agent
             .delete("/api/auth/login")
-            .send();
+            .send().then((res) => {
+                res.should.have.status(200);
+                res.body.should.be.an("object");
+                res.body.should.have.property("success").equal(true);
+            }));
 
-        res.should.have.status(200);
-        res.body.should.be.an("object");
-        res.body.should.have.property("success").equal(true);
-    });
-
-    it("Verify if the log out happen server side", async () => {
-        const res = await this.agent
+        it("Verify if the log out happen server side", () => this.agent
             .get("/api/auth/login")
-            .send();
-
-        res.should.have.status(401);
-        res.body.should.be.an("object");
-        res.body.should.have.property("success").equal(false);
-        res.body.should.have.property("reason").that.is.a("string");
+            .send().then((res) => {
+                res.should.have.status(401);
+                res.body.should.be.an("object");
+                res.body.should.have.property("success").equal(false);
+                res.body.should.have.property("reason").that.is.a("string");
+            }));
     });
+
 });
