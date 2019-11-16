e59a0b4aa58017fd92fe8cbdf6efbfc628976d5b
miguelpduarte <miguelpduarte98@gmail.com>
2019-11-16 13:53:38 +0000

    Add middleware for checking for admin token

--- src/api/middleware/auth.js
+++ src/api/middleware/auth.js
@@ -1,4 +1,5 @@
 const { ErrorTypes } = require("./errorHandler");
+const config = require("../../config/env");
 
 // Middleware to require login in an endpoint
 const authRequired = (req, res, next) => {
@@ -8,8 +9,24 @@ const authRequired = (req, res, next) => {
     return res.status(401).json({
         success: false,
         reason: "Must be logged in",
-        code: ErrorTypes.FORBIDDEN,
+        error_code: ErrorTypes.FORBIDDEN,
     });
 };
 
-module.exports = { authRequired };
+// Eventually should be done via a session in an admin account, but at least this will work for now, before a permission system is added
+const isAdmin = (req, res, next) => {
+    if (req.body.admin_token !== config.admin_token) {
+        return res.status(401).json({
+            success: false,
+            reason: "Invalid admin token",
+            error_code: ErrorTypes.FORBIDDEN,
+        });
+    }
+
+    return next();
+};
+
+module.exports = {
+    authRequired,
+    isAdmin,
+};
