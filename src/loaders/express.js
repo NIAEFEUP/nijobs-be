5a04aae13785aa1c57a3bc31cca093a76b2b52bf
Angelo Teixeira <imnotteixeira@gmail.com>
2020-01-23 12:56:37 +0000

    Added CORS origin from .env or '*' as default

--- src/loaders/express.js
+++ src/loaders/express.js
@@ -43,7 +43,7 @@ module.exports = (app) => {
     // Adding headers (CORS)
     app.use((_, res, next) => {
         // Allow connections for all origins
-        res.setHeader("Access-Control-Allow-Origin", "*");
+        res.setHeader("Access-Control-Allow-Origin", config.access_control_allow_origin);
         // Allowed request methods
         res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
         // Allowed request headers
