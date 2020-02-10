4ced46fc2d2a948f4f1f42336bbe84d3be2fbbca
Guilherme Silva <guilhermejosesilva@gmail.com>
2020-02-10 16:08:41 +0000

    Add routes to loader

--- src/api/index.js
+++ src/api/index.js
@@ -2,12 +2,14 @@ const { Router } = require("express");
 const auth = require("./routes/auth");
 const example = require("./routes/example");
 const offer = require("./routes/offer");
+const application = require("./routes/application");
 
 module.exports = () => {
     const app = Router();
     auth(app);
     example(app);
     offer(app);
+    application(app);
 
     return app;
 };
--- src/api/routes/application.js
+++ src/api/routes/application.js
@@ -2,12 +2,12 @@ const HTTPStatus = require("http-status-codes");
 const { Router } = require("express");
 
 const validators = require("../middleware/validators/application");
-const AuthService = require("../../services/application");
+const ApplicationService = require("../../services/application");
 
 const router = Router();
 
 module.exports = (app) => {
-    app.use("/application", router);
+    app.use("/application/company", router);
 
     /**
      * Creates a new Company Application
@@ -15,9 +15,9 @@ module.exports = (app) => {
     router.post("/", validators.create, async (req, res) => {
         try {
             // This is safe since the service is destructuring the passed object and the fields have been validated
-            const offer = await (new AuthService()).create(req.body);
+            const application = await (new ApplicationService()).create(req.body);
 
-            return res.json(offer);
+            return res.json(application);
         } catch (err) {
             console.error(err);
             return res.status(HTTPStatus.INTERNAL_SERVER_ERROR).send();
