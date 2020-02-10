27087140fc526978a0967ac16d8944cd36025645
Guilherme Silva <guilhermejosesilva@gmail.com>
2020-02-10 15:21:06 +0000

    Add create application validator

---
+++ src/api/middleware/validators/application.js
@@ -0,0 +1,44 @@
+const { body } = require("express-validator");
+
+const { useExpressValidators } = require("../errorHandler");
+const ValidationReasons = require("./validationReasons");
+const Account = require("../../../models/Account");
+
+const checkDuplicatedEmail = async (email) => {
+    const acc = await Account.findOne({ email }).exec();
+    if (acc) {
+        throw new Error(ValidationReasons.ALREADY_EXISTS("email"));
+    }
+};
+
+const create = useExpressValidators([
+    body("email", ValidationReasons.DEFAULT)
+        .exists().withMessage(ValidationReasons.REQUIRED).bail()
+        .normalizeEmail().isEmail().withMessage(ValidationReasons.EMAIL)
+        .bail()
+        .custom(checkDuplicatedEmail)
+        .trim(),
+
+    body("password", ValidationReasons.DEFAULT)
+        .exists().withMessage(ValidationReasons.REQUIRED).bail()
+        .isString().withMessage(ValidationReasons.STRING)
+        .isLength({ min: 8 }).withMessage(ValidationReasons.TOO_SHORT(8))
+        .matches(/\d/).withMessage(ValidationReasons.HAVE_NUMBER),
+
+    body("motivation", ValidationReasons.DEFAULT)
+        .exists().withMessage(ValidationReasons.REQUIRED).bail()
+        .isString().withMessage(ValidationReasons.STRING)
+        .isLength({ max: 1500 }).withMessage(ValidationReasons.TOO_LONG(1500))
+        .isLength({ min: 10 }).withMessage(ValidationReasons.TOO_SHORT(10))
+        .trim(),
+
+    body("companyName", ValidationReasons.DEFAULT)
+        .exists().withMessage(ValidationReasons.REQUIRED).bail()
+        .isString().withMessage(ValidationReasons.STRING)
+        .isLength({ max: 20 }).withMessage(ValidationReasons.TOO_LONG(20))
+        .isLength({ min: 3 }).withMessage(ValidationReasons.TOO_SHORT(3))
+        .trim(),
+]);
+
+
+module.exports = { create };
