6751460d3816acdf7f8aacf6ea04ea3ed2f04bb2
Bruno Rosendo <brunomrosendo@hotmail.com>
2021-02-07 17:00:39 +0000

    Created two tests for job duration

--- test/end-to-end/offer.js
+++ test/end-to-end/offer.js
@@ -479,6 +479,42 @@ describe("Offer endpoint tests", () => {
                 expect(created_offer).toHaveProperty("publishDate");
             });
         });
+
+        describe("Job Duration", () => {
+            test("should fail if jobMinDuration is greater than jobMaxDuration", async () => {
+                const offer_params = generateTestOffer({
+                    jobMinDuration: 10,
+                    jobMaxDuration: 8,
+                    owner: test_company._id,
+                    ownerName: test_company.name,
+                });
+
+                const res = await request()
+                    .post("/offers/new")
+                    .send(withGodToken(offer_params));
+
+                expect(res.status).toBe(HTTPStatus.UNPROCESSABLE_ENTITY);
+                expect(res.body).toHaveProperty("error_code", ErrorTypes.VALIDATION_ERROR);
+                expect(res.body).toHaveProperty("errors");
+                expect(res.body.errors[0]).toHaveProperty("param", "jobMaxDuration");
+                expect(res.body.errors[0]).toHaveProperty("msg", ValidationReasons.MUST_BE_AFTER("jobMinDuration"));
+            });
+
+            test("should succeed if jobMaxDuration is greater than jobMinDuration", async () => {
+                const offer_params = generateTestOffer({
+                    jobMinDuration: 8,
+                    jobMaxDuration: 10,
+                    owner: test_company._id,
+                    ownerName: test_company.name,
+                });
+
+                const res = await request()
+                    .post("/offers/new")
+                    .send(withGodToken(offer_params));
+
+                expect(res.status).toBe(HTTPStatus.OK);
+            });
+        });
     });
 
     describe("GET /offers", () => {
