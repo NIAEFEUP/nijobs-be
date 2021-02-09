852d41010c8a5bea2196bfb732a3f045e1659db1
Bruno Rosendo <brunomrosendo@hotmail.com>
2021-02-09 18:59:17 +0000

    created two new tests for missing job duration fields

--- test/end-to-end/offer.js
+++ test/end-to-end/offer.js
@@ -514,6 +514,38 @@ describe("Offer endpoint tests", () => {
 
                 expect(res.status).toBe(HTTPStatus.OK);
             });
+
+            test("should fail if jobMaxDuration is specified and jobMinDuration isn't", async () => {
+                const offer_params = generateTestOffer({
+                    jobMaxDuration: 8,
+                    owner: test_company._id,
+                    ownerName: test_company.name,
+                });
+
+                const res = await request()
+                    .post("/offers/new")
+                    .send(withGodToken(offer_params));
+
+                expect(res.status).toBe(HTTPStatus.UNPROCESSABLE_ENTITY);
+                expect(res.body).toHaveProperty("error_code", ErrorTypes.VALIDATION_ERROR);
+                expect(res.body).toHaveProperty("errors");
+                expect(res.body.errors[0]).toHaveProperty("param", "jobMaxDuration");
+                expect(res.body.errors[0]).toHaveProperty("msg", ValidationReasons.JOB_MIN_DURATION_NOT_SPECIFIED);
+            });
+
+            test("should succeed if jobMinDuration is specified and jobMaxDuration isn't", async () => {
+                const offer_params = generateTestOffer({
+                    jobMinDuration: 8,
+                    owner: test_company._id,
+                    ownerName: test_company.name,
+                });
+
+                const res = await request()
+                    .post("/offers/new")
+                    .send(withGodToken(offer_params));
+
+                expect(res.status).toBe(HTTPStatus.OK);
+            });
         });
     });
 
