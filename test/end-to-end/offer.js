55c75e817872df8254728a4f7c370d7fa51550ca
Nuno Pereira <nunoafonso2002@gmail.com>
2021-05-01 11:29:36 +0100

    Added tests for general offer GET

--- test/end-to-end/offer.js
+++ test/end-to-end/offer.js
@@ -980,6 +980,102 @@ describe("Offer endpoint tests", () => {
                     });
                 });
 
+                describe("showAdminReason", () => {
+
+                    beforeAll(async () => {
+                        await Offer.deleteMany({});
+                        const test_offers = [];
+
+                        for (let i = 0; i < 5; i++)
+                            test_offers.push(
+                                { ...test_offer,
+                                    isHidden: true,
+                                    hiddenReason: "ADMIN_REQUEST",
+                                    adminReason: "my_reason"
+                                });
+
+                        await Offer.create(test_offers);
+                    });
+
+                    afterEach(async () => {
+                        await test_agent.del("/auth/login");
+                    });
+
+                    test("should return adminReason if logged as admin", async () => {
+
+                        await test_agent
+                            .post("/auth/login")
+                            .send(test_user_admin)
+                            .expect(HTTPStatus.OK);
+
+                        const res = await test_agent
+                            .get("/offers")
+                            .query({
+                                showHidden: true,
+                            });
+
+                        expect(res.status).toBe(HTTPStatus.OK);
+
+                        const extracted_data = res.body.map((elem) => elem["adminReason"]);
+
+                        const expected_data = ["my_reason", "my_reason", "my_reason", "my_reason", "my_reason"];
+
+                        expect(extracted_data).toEqual(expected_data);
+                    });
+
+                    test("should return adminReason if god token is sent", async () => {
+
+                        const res = await test_agent
+                            .get("/offers")
+                            .query({
+                                showHidden: true,
+                            })
+                            .send(withGodToken());
+
+                        expect(res.status).toBe(HTTPStatus.OK);
+
+                        const extracted_data = res.body.map((elem) => elem["adminReason"]);
+
+                        const expected_data = ["my_reason", "my_reason", "my_reason", "my_reason", "my_reason"];
+
+                        expect(extracted_data).toEqual(expected_data);
+                    });
+
+                    test("should not return adminReason if logged as company", async () => {
+
+                        await test_agent
+                            .post("/auth/login")
+                            .send(test_user_company)
+                            .expect(HTTPStatus.OK);
+
+                        const res = await test_agent
+                            .get("/offers");
+
+                        expect(res.status).toBe(HTTPStatus.OK);
+
+                        const extracted_data = res.body.map((elem) => elem["adminReason"]);
+
+                        const expected_data = [];
+
+                        expect(extracted_data).toEqual(expected_data);
+
+                    });
+
+                    test("should not return adminReason if not logged in", async () => {
+
+                        const res = await test_agent
+                            .get("/offers");
+
+                        expect(res.status).toBe(HTTPStatus.OK);
+
+                        const extracted_data = res.body.map((elem) => elem["adminReason"]);
+
+                        const expected_data = [];
+
+                        expect(extracted_data).toEqual(expected_data);
+
+                    });
+                });
             });
 
             describe("Full text search", () => {
