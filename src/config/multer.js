c2d1359f20280a904f5bf8e7d829ae7a27ec8b9c
Bruno Rosendo <brunomrosendo@hotmail.com>
2021-06-19 15:11:19 +0100

    Changed max file size and parsing error the right way

--- src/config/multer.js
+++ src/config/multer.js
@@ -2,7 +2,7 @@ const multer = require("multer");
 const ValidationReasons = require("../api/middleware/validators/validationReasons");
 
 const storage =  multer.memoryStorage();
-const limits = { fileSize: 5000000 };
+const limits = { fileSize: 10485760 };
 const fileFilter = (req, file, cb) => {
     const fileTypes = /png|jpeg|jpg/;
     const mimeType = fileTypes.test(file.mimetype);
