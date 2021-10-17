f26efd35e7630e962fb0ed676530518dbd725850
Bruno Rosendo <brunomrosendo@hotmail.com>
2021-10-17 15:45:34 +0100

    Audit fix and jest bump to v27

--- __mocks__/nodemailer.js
+++ __mocks__/nodemailer.js
@@ -1,7 +1,11 @@
 
 const transporter = {
     verify: jest.fn(),
-    sendMail: jest.fn(),
+    sendMail: jest.fn().mockReturnValue({
+        response: "mockedMail",
+        envelope: "mockedMail",
+        messageId: "mock123"
+    }),
     use: jest.fn()
 };
 const createTransport = jest.fn().mockReturnValue(transporter);
