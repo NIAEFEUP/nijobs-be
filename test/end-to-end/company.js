884d3a37d71a2c4bfb6cb5c6f90cc456f52394e4
Bruno Rosendo <brunomrosendo@hotmail.com>
2021-12-05 22:07:15 +0000

    Created tests for the concurrent offers endpoint

--- test/end-to-end/company.js
+++ test/end-to-end/company.js
@@ -1664,4 +1664,262 @@ describe("Company endpoint", () => {
             }));
         });
     });
+
+    describe("GET /company/:companyId/concurrent", () => {
+        let test_company_1, test_company_2;
+        const test_user_admin = {
+            email: "admin@email.com",
+            password: "password123",
+        };
+        const test_user_company_1 = {
+            email: "company1@email.com",
+            password: "password123",
+        };
+        const test_user_company_2 = {
+            email: "company2@email.com",
+            password: "password123",
+        };
+
+        const test_agent = agent();
+
+        const publishDate = (new Date(Date.now() - (2 * DAY_TO_MS))).toISOString();
+        const publishEndDate = (new Date(Date.now() + (2 * DAY_TO_MS))).toISOString();
+
+        beforeEach(async () => {
+            await test_agent
+                .delete("/auth/login")
+                .expect(HTTPStatus.OK);
+
+            await Company.deleteMany({});
+
+            [test_company_1, test_company_2] = await Company.create([
+                {
+                    name: "test-company-1",
+                    hasFinishedRegistration: true
+                }, {
+                    name: "test-company-2",
+                    hasFinishedRegistration: true,
+                    logo: "http://oniebuedafixe.com/wow.png"
+                }
+            ]);
+
+            await Account.deleteMany({});
+            await Account.create({
+                email: test_user_admin.email,
+                password: await hash(test_user_admin.password),
+                isAdmin: true
+            });
+            await Account.create({
+                email: test_user_company_1.email,
+                password: await hash(test_user_company_1.password),
+                company: test_company_1._id
+            });
+            await Account.create({
+                email: test_user_company_2.email,
+                password: await hash(test_user_company_2.password),
+                company: test_company_2._id
+            });
+
+            const testOffers = Array(CompanyConstants.offers.max_concurrent)
+                .fill(generateTestOffer({
+                    owner: test_company_2._id,
+                    ownerName: test_company_2.name,
+                    ownerLogo: test_company_2.logo,
+                    "publishDate": (new Date(Date.now() - (DAY_TO_MS))).toISOString(),
+                    "publishEndDate": (new Date(Date.now() + (DAY_TO_MS))).toISOString()
+                }));
+
+            await Offer.deleteMany({});
+            await Offer.create(testOffers);
+        });
+
+        afterAll(async () => {
+            await Account.deleteMany({});
+            await Company.deleteMany({});
+            await Offer.deleteMany({});
+        });
+
+        describe("Id validation", () => {
+            test("Should fail if using an invalid id", async () => {
+
+                const res = await test_agent
+                    .get("/company/123/concurrent")
+                    .send(withGodToken({ publishDate, publishEndDate }))
+                    .expect(HTTPStatus.UNPROCESSABLE_ENTITY);
+
+                expect(res.body).toHaveProperty("error_code", ErrorTypes.VALIDATION_ERROR);
+                expect(res.body).toHaveProperty("errors");
+                expect(res.body.errors[0]).toHaveProperty("param", "companyId");
+                expect(res.body.errors[0]).toHaveProperty("msg", ValidationReasons.OBJECT_ID);
+            });
+
+            test("Should fail if company does not exist", async () => {
+
+                const id = "111111111111111111111111";
+                const res = await test_agent
+                    .get(`/company/${id}/concurrent`)
+                    .send(withGodToken({ publishDate, publishEndDate }))
+                    .expect(HTTPStatus.UNPROCESSABLE_ENTITY);
+
+                expect(res.body).toHaveProperty("error_code", ErrorTypes.VALIDATION_ERROR);
+                expect(res.body).toHaveProperty("errors");
+                expect(res.body.errors[0]).toHaveProperty("param", "companyId");
+                expect(res.body.errors[0]).toHaveProperty("msg", ValidationReasons.COMPANY_NOT_FOUND(id));
+            });
+        });
+
+        describe("Date validation", () => {
+            test("Should fail if publishDate is not specified", async () => {
+
+                const res = await test_agent
+                    .get(`/company/${test_company_1._id}/concurrent`)
+                    .send(withGodToken({ publishEndDate }))
+                    .expect(HTTPStatus.UNPROCESSABLE_ENTITY);
+
+                expect(res.body).toHaveProperty("error_code", ErrorTypes.VALIDATION_ERROR);
+                expect(res.body).toHaveProperty("errors");
+                expect(res.body.errors[0]).toHaveProperty("param", "publishDate");
+                expect(res.body.errors[0]).toHaveProperty("msg", ValidationReasons.REQUIRED);
+            });
+
+            test("Should fail if publishEndDate is not specified", async () => {
+
+                const res = await test_agent
+                    .get(`/company/${test_company_1._id}/concurrent`)
+                    .send(withGodToken({ publishDate }))
+                    .expect(HTTPStatus.UNPROCESSABLE_ENTITY);
+
+                expect(res.body).toHaveProperty("error_code", ErrorTypes.VALIDATION_ERROR);
+                expect(res.body).toHaveProperty("errors");
+                expect(res.body.errors[0]).toHaveProperty("param", "publishEndDate");
+                expect(res.body.errors[0]).toHaveProperty("msg", ValidationReasons.REQUIRED);
+            });
+
+            test("Should fail if publishDate is after publishEndDate", async () => {
+
+                const res = await test_agent
+                    .get(`/company/${test_company_1._id}/concurrent`)
+                    .send(withGodToken({
+                        publishDate: publishEndDate,
+                        publishEndDate: publishDate,
+                    }))
+                    .expect(HTTPStatus.UNPROCESSABLE_ENTITY);
+
+                expect(res.body).toHaveProperty("error_code", ErrorTypes.VALIDATION_ERROR);
+                expect(res.body).toHaveProperty("errors");
+                expect(res.body.errors[0]).toHaveProperty("param", "publishEndDate");
+                expect(res.body.errors[0]).toHaveProperty("msg", ValidationReasons.MUST_BE_AFTER("publishDate"));
+            });
+
+            test("Should fail if publishDate doesn't have a date format", async () => {
+
+                const res = await test_agent
+                    .get(`/company/${test_company_1._id}/concurrent`)
+                    .send(withGodToken({ publishDate: "123", publishEndDate }))
+                    .expect(HTTPStatus.UNPROCESSABLE_ENTITY);
+
+                expect(res.body).toHaveProperty("error_code", ErrorTypes.VALIDATION_ERROR);
+                expect(res.body).toHaveProperty("errors");
+                expect(res.body.errors[0]).toHaveProperty("param", "publishDate");
+                expect(res.body.errors[0]).toHaveProperty("msg", ValidationReasons.DATE);
+            });
+
+            test("Should fail if publishEndDate doesn't have a date format", async () => {
+
+                const res = await test_agent
+                    .get(`/company/${test_company_1._id}/concurrent`)
+                    .send(withGodToken({ publishDate, publishEndDate: "123" }))
+                    .expect(HTTPStatus.UNPROCESSABLE_ENTITY);
+
+                expect(res.body).toHaveProperty("error_code", ErrorTypes.VALIDATION_ERROR);
+                expect(res.body).toHaveProperty("errors");
+                expect(res.body.errors[0]).toHaveProperty("param", "publishEndDate");
+                expect(res.body.errors[0]).toHaveProperty("msg", ValidationReasons.DATE);
+            });
+        });
+
+        test("Should fail if not logged in", async () => {
+
+            const res = await test_agent
+                .get(`/company/${test_company_1._id}/concurrent`)
+                .send({ publishDate, publishEndDate })
+                .expect(HTTPStatus.UNAUTHORIZED);
+
+            expect(res.body).toHaveProperty("error_code", ErrorTypes.FORBIDDEN);
+            expect(res.body).toHaveProperty("errors");
+            expect(res.body.errors[0]).toHaveProperty("msg", ValidationReasons.INSUFFICIENT_PERMISSIONS);
+        });
+
+        test("Should fail if logged as a different company", async () => {
+
+            await test_agent
+                .post("/auth/login")
+                .send(test_user_company_2)
+                .expect(HTTPStatus.OK);
+
+            const res = await test_agent
+                .get(`/company/${test_company_1._id}/concurrent`)
+                .send({ publishDate, publishEndDate })
+                .expect(HTTPStatus.FORBIDDEN);
+
+            expect(res.body).toHaveProperty("error_code", ErrorTypes.FORBIDDEN);
+            expect(res.body).toHaveProperty("errors");
+            expect(res.body.errors[0]).toHaveProperty("msg", ValidationReasons.INSUFFICIENT_PERMISSIONS_COMPANY_VISIBILITY);
+        });
+
+        test("Should succeed if god token is sent", async () => {
+
+            const res = await test_agent
+                .get(`/company/${test_company_1._id}/concurrent`)
+                .send(withGodToken({ publishDate, publishEndDate }))
+                .expect(HTTPStatus.OK);
+
+            expect(res.body).toHaveProperty("maxOffersReached", false);
+        });
+
+        test("Should succeed if logged as an admin", async () => {
+
+            await test_agent
+                .post("/auth/login")
+                .send(test_user_admin)
+                .expect(HTTPStatus.OK);
+
+            const res = await test_agent
+                .get(`/company/${test_company_1._id}/concurrent`)
+                .send({ publishDate, publishEndDate })
+                .expect(HTTPStatus.OK);
+
+            expect(res.body).toHaveProperty("maxOffersReached", false);
+        });
+
+        test("Should succeed if logged as the same company", async () => {
+
+            await test_agent
+                .post("/auth/login")
+                .send(test_user_company_1)
+                .expect(HTTPStatus.OK);
+
+            const res = await test_agent
+                .get(`/company/${test_company_1._id}/concurrent`)
+                .send({ publishDate, publishEndDate })
+                .expect(HTTPStatus.OK);
+
+            expect(res.body).toHaveProperty("maxOffersReached", false);
+        });
+
+        test("Should return true if the company has reached max offers in the time interval", async () => {
+
+            await test_agent
+                .post("/auth/login")
+                .send(test_user_company_2)
+                .expect(HTTPStatus.OK);
+
+            const res = await test_agent
+                .get(`/company/${test_company_2._id}/concurrent`)
+                .send({ publishDate, publishEndDate })
+                .expect(HTTPStatus.OK);
+
+            expect(res.body).toHaveProperty("maxOffersReached", true);
+        });
+    });
 });
