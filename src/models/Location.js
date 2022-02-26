d4b89303ff766df3e0d7d317a817fa7e2d0029f5
Nuno Pereira <nunoafonso2002@gmail.com>
2022-02-26 17:40:13 +0000

    Added Location model

---
+++ src/models/Location.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+
+const LocationSchema = new Schema({
+    city: {
+        type: String,
+        required: true
+    },
+    cityAscii: {
+        type: String,
+        required: true
+    },
+    country: {
+        type: String,
+        required: true
+    },
+    latitude: {
+        type: Number,
+        required: true
+    },
+    longitude: {
+        type: Number,
+        required: true
+    }
+});
+
+LocationSchema.index(
+    { city: "text", cityAscii: "text", country: "text", latitude: "number", longitude: "number" },
+    { name: "Search index", weights: { city: 10, cityAscii: 10, country: 5, latitude: 5, longitude: 5 } }
+);
+
+const Location = mongoose.model("Location", LocationSchema);
+
+export default Location;
