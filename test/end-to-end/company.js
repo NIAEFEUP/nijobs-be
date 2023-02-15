0fb2bdb11b381963029ae67b27ebf8c40be8b44b
toino <joaoapereira21@hotmail.com>
2023-02-15 12:33:01 +0000

    Make tests expect a 404 when a model is not found

--- test/end-to-end/company.js
+++ test/end-to-end/company.js
@@ -571,7 +571,7 @@ describe("Company endpoint", () => {
             const res = await test_agent
                 .put(`/company/${id}/block`)
                 .send({ adminReason })
-                .expect(HTTPStatus.UNPROCESSABLE_ENTITY);
+                .expect(HTTPStatus.NOT_FOUND);
             expect(res.body).toHaveProperty("error_code", ErrorTypes.VALIDATION_ERROR);
             expect(res.body).toHaveProperty("errors");
             expect(res.body.errors[0]).toHaveProperty("param", "companyId");
@@ -812,7 +812,7 @@ describe("Company endpoint", () => {
             const id = "111111111111111111111111";
             const res = await test_agent
                 .put(`/company/${id}/unblock`)
-                .expect(HTTPStatus.UNPROCESSABLE_ENTITY);
+                .expect(HTTPStatus.NOT_FOUND);
             expect(res.body).toHaveProperty("error_code", ErrorTypes.VALIDATION_ERROR);
             expect(res.body).toHaveProperty("errors");
             expect(res.body.errors[0]).toHaveProperty("param", "companyId");
@@ -1123,7 +1123,7 @@ describe("Company endpoint", () => {
                 const res = await test_agent
                     .put(`/company/${id}/enable`)
                     .send(withGodToken())
-                    .expect(HTTPStatus.UNPROCESSABLE_ENTITY);
+                    .expect(HTTPStatus.NOT_FOUND);
 
                 expect(res.body).toHaveProperty("error_code", ErrorTypes.VALIDATION_ERROR);
                 expect(res.body).toHaveProperty("errors");
@@ -1368,7 +1368,7 @@ describe("Company endpoint", () => {
                 const res = await test_agent
                     .put(`/company/${id}/disable`)
                     .send(withGodToken())
-                    .expect(HTTPStatus.UNPROCESSABLE_ENTITY);
+                    .expect(HTTPStatus.NOT_FOUND);
 
                 expect(res.body).toHaveProperty("error_code", ErrorTypes.VALIDATION_ERROR);
                 expect(res.body).toHaveProperty("errors");
@@ -1567,7 +1567,7 @@ describe("Company endpoint", () => {
                 const res = await test_agent
                     .post(`/company/${id}/delete`)
                     .send(withGodToken())
-                    .expect(HTTPStatus.UNPROCESSABLE_ENTITY);
+                    .expect(HTTPStatus.NOT_FOUND);
 
                 expect(res.body).toHaveProperty("error_code", ErrorTypes.VALIDATION_ERROR);
                 expect(res.body).toHaveProperty("errors");
@@ -1766,7 +1766,7 @@ describe("Company endpoint", () => {
                 const res = await test_agent
                     .get(`/company/${id}/hasReachedMaxConcurrentOffersBetweenDates`)
                     .send(withGodToken({ publishDate, publishEndDate }))
-                    .expect(HTTPStatus.UNPROCESSABLE_ENTITY);
+                    .expect(HTTPStatus.NOT_FOUND);
 
                 expect(res.body).toHaveProperty("error_code", ErrorTypes.VALIDATION_ERROR);
                 expect(res.body).toHaveProperty("errors");
--- test/end-to-end/offer.js
+++ test/end-to-end/offer.js
@@ -3025,7 +3025,7 @@ describe("Offer endpoint tests", () => {
                 const res = await test_agent
                     .post(`/offers/edit/${_id}`)
                     .send(withGodToken())
-                    .expect(HTTPStatus.UNPROCESSABLE_ENTITY);
+                    .expect(HTTPStatus.NOT_FOUND);
                 expect(res.body.errors[0]).toHaveProperty("param", "offerId");
                 expect(res.body.errors[0]).toHaveProperty("msg", ValidationReasons.OFFER_NOT_FOUND(_id));
             });
@@ -4501,7 +4501,7 @@ describe("Offer endpoint tests", () => {
             const res = await test_agent
                 .put(`/offers/${_id}/archive`)
                 .send(withGodToken())
-                .expect(HTTPStatus.UNPROCESSABLE_ENTITY);
+                .expect(HTTPStatus.NOT_FOUND);
 
             expect(res.body.errors[0]).toHaveProperty("param", "offerId");
             expect(res.body.errors[0]).toHaveProperty("msg", ValidationReasons.OFFER_NOT_FOUND(_id));
