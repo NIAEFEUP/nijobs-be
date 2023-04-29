08cd06e5b7407706ddbd992a44740c2125af4b38
Nuno Pereira <nunoafonso2002@gmail.com>
2023-04-29 16:28:30 +0100

    Restructured application finish tests

--- test/end-to-end/company/application/finish.js
+++ test/end-to-end/company/application/finish.js
@@ -16,97 +16,14 @@ describe("POST /company/application/finish", () => {
 
     const test_agent = agent();
 
-    const testUserAdmin = {
-        email: "admin@email.com",
-        password: "password123",
-    };
-
-    const testUser = {
-        email: "user@email.com",
-        password: "password123",
-    };
-    const nonFinishedCompanyData = {
-        name: "Company Ltd",
-    };
-    let testCompany;
-
-    const testSingleContactUser = {
-        email: "userSingleContact@email.com",
-        password: "password123",
-    };
-    const nonFinishedSingleContactCompanyData = {
-        name: "Company Ltd2",
-    };
-    let testSingleContactCompany;
-
-    const testFinishedUser = {
-        email: "finishedUsser@email.com",
-        password: "password123",
-    };
-    const finishedCompanyData = {
-        name: "Company Ltd",
-        hasFinishedRegistration: true,
-    };
-
     beforeAll(async () => {
         await Company.deleteMany({});
         await Account.deleteMany({});
-
-        await Account.create({
-            email: testUserAdmin.email,
-            password: await hash(testUserAdmin.password),
-            isAdmin: true,
-        });
-
-        const [
-            _testCompany,
-            testFinishedCompany,
-            _testSingleContactCompany,
-        ] = await Company.create([
-            nonFinishedCompanyData,
-            finishedCompanyData,
-            nonFinishedSingleContactCompanyData,
-        ]);
-        testCompany = _testCompany;
-        testSingleContactCompany = _testSingleContactCompany;
-
-        await Account.create({
-            email: testUser.email,
-            password: await hash(testUser.password),
-            company: testCompany._id
-        });
-
-        await Account.create({
-            email: testFinishedUser.email,
-            password: await hash(testFinishedUser.password),
-            company: testFinishedCompany._id
-        });
-
-        await Account.create({
-            email: testSingleContactUser.email,
-            password: await hash(testSingleContactUser.password),
-            company: testSingleContactCompany._id
-        });
     });
 
     afterAll(async () => {
         await Company.deleteMany({});
         await Account.deleteMany({});
-    });
-
-    beforeEach(async () => {
-        // Login
-        await test_agent
-            .post("/auth/login")
-            .send(testUser)
-            .expect(StatusCodes.OK);
-    });
-
-    afterEach(async () => {
-        // Logout
-        await test_agent
-            .delete("/auth/login")
-            .expect(StatusCodes.OK);
     });
 
     describe("Input Validation", () => {
@@ -215,125 +132,221 @@ describe("POST /company/application/finish", () => {
         });
     });
 
-    test("should fail if making unauthenticated request", async () => {
-
-        await test_agent
-            .delete("/auth/login")
-            .expect(StatusCodes.OK);
-
-        await request()
-            .post("/company/application/finish")
-            .expect(StatusCodes.UNAUTHORIZED);
+    describe("Without Auth", () => {
+        test("should fail if making unauthenticated request", async () => {
+            await test_agent
+                .post("/company/application/finish")
+                .expect(StatusCodes.UNAUTHORIZED);
+        });
     });
 
-    test("should fail if authenticated as admin", async () => {
-        // Login
-        await test_agent
-            .post("/auth/login")
-            .send(testUserAdmin)
-            .expect(StatusCodes.OK);
+    describe("With Auth", () => {
 
-        await request()
-            .post("/company/application/finish")
-            .expect(StatusCodes.UNAUTHORIZED);
-    });
+        const testUserAdmin = {
+            email: "admin@email.com",
+            password: "password123",
+        };
 
-    test("should fail if sending god token", async () => {
+        const testFinishedUser = {
+            email: "finishedUsser@email.com",
+            password: "password123",
+        };
+        const finishedCompanyData = {
+            name: "Company Ltd",
+            hasFinishedRegistration: true,
+        };
 
-        await test_agent
-            .delete("/auth/login")
-            .expect(StatusCodes.OK);
+        beforeAll(async () => {
+            await Account.create({
+                email: testUserAdmin.email,
+                password: await hash(testUserAdmin.password),
+                isAdmin: true,
+            });
 
-        await test_agent
-            .post("/company/application/finish")
-            .send(withGodToken())
-            .expect(StatusCodes.UNAUTHORIZED);
-    });
+            const testFinishedCompany = await Company.create(finishedCompanyData);
+            await Account.create({
+                email: testFinishedUser.email,
+                password: await hash(testFinishedUser.password),
+                company: testFinishedCompany._id
+            });
+        });
 
-    test("should fail if company has already finished registration", async () => {
-        // Login
-        await test_agent
-            .post("/auth/login")
-            .send(testFinishedUser)
-            .expect(StatusCodes.OK);
+        afterAll(async () => {
+            await Company.deleteMany({ name: finishedCompanyData.name });
+            await Account.deleteMany({ isAdmin: true });
+            await Account.deleteMany({ email: testFinishedUser.email });
+        });
 
-        const res = await test_agent
-            .post("/company/application/finish")
-            .expect(StatusCodes.FORBIDDEN);
+        afterEach(async () => {
+            // Logout
+            await test_agent
+                .delete("/auth/login")
+                .expect(StatusCodes.OK);
+        });
 
-        expect(res.body).toHaveProperty("errors", expect.arrayContaining(
-            [
-                expect.objectContaining({
-                    "msg": ValidationReasons.REGISTRATION_FINISHED,
-                }),
-            ]
-        ));
-    });
+        test("should fail if authenticated as admin", async () => {
+            // Login
+            await test_agent
+                .post("/auth/login")
+                .send(testUserAdmin)
+                .expect(StatusCodes.OK);
 
-    test("should finish the application with multiple contacts", async () => {
+            await request()
+                .post("/company/application/finish")
+                .expect(StatusCodes.UNAUTHORIZED);
+        });
 
-        const contacts = ["contact1", "contact2"];
+        test("should fail if sending god token", async () => {
 
-        await test_agent
-            .post("/company/application/finish")
-            .attach("logo", "test/data/logo-niaefeup.png")
-            .field("bio", "A very interesting and compelling bio")
-            .field("contacts", contacts)
-            .expect(StatusCodes.OK);
+            await test_agent
+                .delete("/auth/login")
+                .expect(StatusCodes.OK);
 
-        const test_companies = await Company.find({ hasFinishedRegistration: true });
-        expect(test_companies).toHaveLength(2);
-        expect(test_companies).toEqual(expect.arrayContaining(
-            [
-                expect.objectContaining({
-                    name: testCompany.name,
-                    hasFinishedRegistration: true,
-                    bio: "A very interesting and compelling bio",
-                    contacts,
-                }),
-            ]
-        ));
+            await test_agent
+                .post("/company/application/finish")
+                .send(withGodToken())
+                .expect(StatusCodes.UNAUTHORIZED);
+        });
 
-        const filename = path.join(`${config.upload_folder}/${testCompany.id}.png`);
-        expect(fs.existsSync(filename)).toBe(true); // TODO: change to async
+        test("should fail if company has already finished registration", async () => {
+            // Login
+            await test_agent
+                .post("/auth/login")
+                .send(testFinishedUser)
+                .expect(StatusCodes.OK);
 
-        // clean up file created
-        await fs.promises.unlink(filename);
-    });
+            const res = await test_agent
+                .post("/company/application/finish")
+                .expect(StatusCodes.FORBIDDEN);
 
-    test("should finish the application with single contact", async () => {
+            expect(res.body).toHaveProperty("errors", expect.arrayContaining(
+                [
+                    expect.objectContaining({
+                        "msg": ValidationReasons.REGISTRATION_FINISHED,
+                    }),
+                ]
+            ));
+        });
 
-        await test_agent
-            .post("/auth/login")
-            .send(testSingleContactUser)
-            .expect(StatusCodes.OK);
+        describe("Contacts", () => {
 
-        const contacts = ["contact1"];
+            const testUser = {
+                email: "user@email.com",
+                password: "password123",
+            };
+            const nonFinishedCompanyData = {
+                name: "Company Ltd",
+            };
+            let testCompany;
 
-        await test_agent
-            .post("/company/application/finish")
-            .attach("logo", "test/data/logo-niaefeup.png")
-            .field("bio", "A very interesting and compelling bio")
-            .field("contacts", contacts)
-            .expect(StatusCodes.OK);
+            const testSingleContactUser = {
+                email: "userSingleContact@email.com",
+                password: "password123",
+            };
+            const nonFinishedSingleContactCompanyData = {
+                name: "Company Ltd2",
+            };
+            let testSingleContactCompany;
 
-        const test_companies = await Company.find({ hasFinishedRegistration: true });
-        expect(test_companies).toHaveLength(3);
-        expect(test_companies).toEqual(expect.arrayContaining(
-            [
-                expect.objectContaining({
-                    name: testSingleContactCompany.name,
-                    hasFinishedRegistration: true,
-                    bio: "A very interesting and compelling bio",
-                    contacts,
-                }),
-            ]
-        ));
+            beforeAll(async () => {
+                [
+                    testCompany,
+                    testSingleContactCompany
+                ] = await Company.create([
+                    nonFinishedCompanyData,
+                    nonFinishedSingleContactCompanyData
+                ]);
 
-        const filename = path.join(`${config.upload_folder}/${testSingleContactCompany.id}.png`);
-        expect(fs.existsSync(filename)).toBe(true); // TODO: change to async
+                await Account.create([
+                    {
+                        email: testUser.email,
+                        password: await hash(testUser.password),
+                        company: testCompany._id
+                    },
+                    {
+                        email: testSingleContactUser.email,
+                        password: await hash(testSingleContactUser.password),
+                        company: testSingleContactCompany._id
+                    },
+                ]);
+            });
 
-        // clean up file created
-        await fs.promises.unlink(filename);
+            afterAll(async () => {
+                await Company.deleteMany({ name: nonFinishedCompanyData.name });
+                await Company.deleteMany({ name: nonFinishedSingleContactCompanyData.name });
+                await Account.deleteMany({ email: testUser.email });
+                await Account.deleteMany({ email: testSingleContactUser.email });
+            });
+
+            test("should finish the application with multiple contacts", async () => {
+                await test_agent
+                    .post("/auth/login")
+                    .send(testUser)
+                    .expect(StatusCodes.OK);
+
+                const contacts = ["contact1", "contact2"];
+
+                await test_agent
+                    .post("/company/application/finish")
+                    .attach("logo", "test/data/logo-niaefeup.png")
+                    .field("bio", "A very interesting and compelling bio")
+                    .field("contacts", contacts)
+                    .expect(StatusCodes.OK);
+
+                const test_companies = await Company.find({ hasFinishedRegistration: true });
+                expect(test_companies).toHaveLength(2);
+                expect(test_companies).toEqual(expect.arrayContaining(
+                    [
+                        expect.objectContaining({
+                            name: testCompany.name,
+                            hasFinishedRegistration: true,
+                            bio: "A very interesting and compelling bio",
+                            contacts,
+                        }),
+                    ]
+                ));
+
+                const filename = path.join(`${config.upload_folder}/${testCompany.id}.png`);
+                expect(fs.existsSync(filename)).toBe(true); // TODO: change to async
+
+                // clean up file created
+                await fs.promises.unlink(filename);
+            });
+
+            test("should finish the application with single contact", async () => {
+                await test_agent
+                    .post("/auth/login")
+                    .send(testSingleContactUser)
+                    .expect(StatusCodes.OK);
+
+                const contacts = ["contact1"];
+
+                await test_agent
+                    .post("/company/application/finish")
+                    .attach("logo", "test/data/logo-niaefeup.png")
+                    .field("bio", "A very interesting and compelling bio")
+                    .field("contacts", contacts)
+                    .expect(StatusCodes.OK);
+
+                const test_companies = await Company.find({ hasFinishedRegistration: true });
+                expect(test_companies).toHaveLength(3);
+                expect(test_companies).toEqual(expect.arrayContaining(
+                    [
+                        expect.objectContaining({
+                            name: testSingleContactCompany.name,
+                            hasFinishedRegistration: true,
+                            bio: "A very interesting and compelling bio",
+                            contacts,
+                        }),
+                    ]
+                ));
+
+                const filename = path.join(`${config.upload_folder}/${testSingleContactCompany.id}.png`);
+                expect(fs.existsSync(filename)).toBe(true); // TODO: change to async
+
+                // clean up file created
+                await fs.promises.unlink(filename);
+            });
+        });
     });
 });
