795afbcfacaf8eb232569744cf4f88d6e4b08d99
marco-vb <marcofilipe03@hotmail.com>
2023-05-01 23:17:08 +0100

    Updated fileMiddleware to return next() in case no image/logo is found (it is checked by the previous validator, so this should not cause problems

--- src/api/routes/company.js
+++ src/api/routes/company.js
@@ -236,6 +236,8 @@ export default (app) => {
         ], { status_code: HTTPStatus.UNAUTHORIZED, error_code: ErrorTypes.FORBIDDEN, msg: ValidationReasons.INSUFFICIENT_PERMISSIONS }),
         validators.edit,
         fileMiddleware.parseSingleFile("logo", false),
+        fileMiddleware.localSave,
+        fileMiddleware.cloudSave,
         (req, res, next) => companyMiddleware.canManageAccountSettings(req.params.companyId)(req, res, next),
         (req, res, next) => companyMiddleware.isNotBlocked(req.params.companyId)(req, res, next),
         (req, res, next) => companyMiddleware.isNotDisabled(req.params.companyId)(req, res, next),
